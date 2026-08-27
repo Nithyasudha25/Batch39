dca17175d4d558103fd8ce7d080801f59f0d5363
Nithyasudha25 <nithyasrim2503@gmail.com>
2026-08-27 12:04:03 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,21 @@
+let studentName = "Nithya";
+let age = 21;
+let mark = 85;
+ console.log(studentName);
+console.log( age);
+console.log( mark);
+
+
+let name = prompt("Enter your name:");
+alert("Hello " + name);
+
+
+let userAge = prompt("Enter your age:");
+console.log("Your age is:", userAge);
+
+let answer = confirm("Do you like watching movie?");
+console.log(answer);
+
+let userName = prompt("Enter your name:");
+document.writeln("Hello " + userName);
+
