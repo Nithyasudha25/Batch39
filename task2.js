17941f4b7e1dfd23e54a845cd72e99db69418137
Nithyasudha25 <nithyasrim2503@gmail.com>
2026-09-01 12:03:22 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,216 @@
+// task 1
+
+// var name = "Nithyasri";
+// let age = 21;
+// const collageName = "SRM TRP ENGINEERING COLLEGE";
+// console.log(name);
+// console.log(age);
+// console.log(collageName);
+// name ="sudhagar";
+// console.log(name);
+// age=32;
+// console.log(age);
+// collageName="Indian institute of technology";
+// console.log(collageName);
+// var name="kavya";
+// console.log(name);
+// let age=25;
+// console.log(age);
+
+// task 2
+
+// var name="what is your name?";
+// name = prompt(name);
+// var age="what is your age?";
+// age = prompt(age);
+// var city="what is your city?";
+// city = prompt(city);
+// document.writeln("Name: " + name + "<br>");
+// document.writeln("Age: " + age + "<br>");
+// document.writeln("City: " + city + "<br>");
+
+
+// task 3
+
+// var userName = prompt("Enter your name:");
+// alert("Welcome, " + userName + "!");
+
+// task 4
+
+// var birthYear = prompt("Enter your birth year:");
+// var currentYear = new Date().getFullYear();
+// var age = currentYear - birthYear;
+// console.log("Birth year: " + birthYear);
+// console.log(" Age : " + age);
+
+// task5
+
+// let a = "Hello";
+// console.log(typeof a);
+// let b = 100;
+// console.log(typeof b);
+// let c = 25.5;
+// console.log(typeof c);
+// let d = true;
+// console.log(typeof d);
+// let e = false;
+// console.log(typeof e);
+// let empty;
+// console.log(empty);
+// let f=prompt();
+// console.log(f);
+
+
+// task 6
+
+// let employee={
+//     name:"Nithya",
+//     age:23,
+// qualification:"B.E",
+// isStudent:false
+// }
+// console.log(employee.name);
+// console.log(employee.age);
+// console.log(employee.qualification);
+// console.log(employee.isStudent);
+
+
+// task 7
+
+// let fruits=["Apple","Mango","Orange","Banana","Grapes","Papaya"];
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[fruits.length - 1]);
+// console.log(fruits.length);
+
+// task 8
+
+// let a=20;
+// let b=5;
+// document.writeln("Addition: " + (a + b) + "<br>");
+// document.writeln("Subtraction: " + (a - b) + "<br>");
+// document.writeln("Multiplication: " + (a * b) + "<br>");
+// document.writeln("Division: " + (a / b) + "<br>");
+// document.writeln("Modulus: " + (a % b) + "<br>");
+// document.writeln("Exponentiation: " + (a ** b) + "<br>");   
+
+// task 9
+
+// let shirt=999;
+// let pant=1499;
+// let shoes=1999;
+// let totalprice=shirt+pant+shoes;
+// document.writeln("Total: " + totalprice);
+
+// task 10
+
+// let tamil=80;
+// let english=75;
+// let maths=90;
+// let totalMarks=tamil+english+maths;
+// let averageMarks=totalMarks/3;
+// document.writeln("Total Marks: " + totalMarks + "<br>");
+// document.writeln("Average Marks: " + averageMarks);
+
+// task 11
+// 11
+// 10
+
+// let a = 10;
+// let b = a++;
+// console.log(a);
+// console.log(b);
+
+// task 12
+// 11
+// 11
+
+// let a = 10;
+// let b = ++a;
+// console.log(a);
+// console.log(b);
+
+// task 13
+
+// let a = 20;
+// let b = a--;
+// console.log(a);
+// console.log(b);
+
+// task 14
+
+// let a = 20;
+// let b = --a;
+// console.log(a);
+// console.log(b);
+
+// task 15
+// 7
+// 4
+// 7
+// 5
+// let a = 5;
+// let b = a++;
+// let c = ++a;
+// let d = b--;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// task 16
+
+// let num = 10;
+// num+=5;
+// console.log(num);
+// num-=5;
+// console.log(num);
+// num*=5;
+// console.log(num);
+// num/=5;
+// console.log(num);
+// num%=5;
+// console.log(num);
+// num**=5;
+// console.log(num);
+
+// task 17
+
+// var name="Nithya";
+// var age=23;
+// var city="Vridhachalam";
+// var college="SRM TRP ENGINEERING COLLEGE";
+// let subject=["Maths","Science","Social Science","English","Tamil"];
+// let student={
+//     name,
+//     age,
+//     city,
+//     college,
+//     subject,
+//     isStudent:true
+// }
+// document.writeln("Name: " + student.name + "<br>");
+// document.writeln("Age: " + student.age + "<br>");
+// document.writeln("City: " + student.city + "<br>");
+// document.writeln("College: " + student.college + "<br>");
+// document.writeln("first subject: " + student.subject[0] + "<br>");
+// document.writeln("last subject: " + student.subject[student.subject.length - 1] + "<br>");
+// document.writeln("total subjects: " + student.subject.length + "<br>");
+// document.writeln("Is student: " + student.isStudent + "<br>");
+
+// final task
+
+var a = Number(prompt("enter a :"));
+var b = Number(prompt("enter b :"));
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+let modulus = a % b;
+let power = a ** b;
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+console.log("Power:", power);
